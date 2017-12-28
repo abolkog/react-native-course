9f9d9da120c26816ddca93728890e34d04b26d64
Khalid Elshafie <abolkog@gmail.com>
2017-12-28 22:29:39 +0900

    Making HTTP Requests

--- books/src/components/BooksList.js
+++ books/src/components/BooksList.js
@@ -1,12 +1,19 @@
 // Step 1: Import Libraries that we need
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-
+import axios from 'axios';
 
 // Step2: Create the component
 class BooksList extends Component {
     componentWillMount() {
         // Calls to the server
+        const url = 'https://abolkog.com/api/books';
+        
+        axios.get(url)
+            .then((response) => {
+                const books = response.data.books;
+                console.log(books);
+            });
     }
 
     render() {
