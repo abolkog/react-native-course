d918e8c87bbe43f6a4ee69a6365fe8c530aa6882
Khalid Elshafie <abolkog@gmail.com>
2017-12-31 23:16:00 +0900

    Adding redux

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,6 +1,10 @@
 //import liraries
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import reducer from './reducers';
 
 import Routes from './RootNavigator';
 
@@ -8,7 +12,9 @@ import Routes from './RootNavigator';
 class App extends Component {
     render() {
         return (
-            <Routes />
+            <Provider store={createStore(reducer)}>
+                <Routes />
+            </Provider>
         );
     }
 }
