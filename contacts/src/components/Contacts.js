57f938c59edcc3ccaadc8079dd9cec4377fb670b
Khalid Elshafie <abolkog@gmail.com>
2018-01-01 00:36:37 +0900

    Styling Contact List

--- contacts/src/components/Contacts.js
+++ contacts/src/components/Contacts.js
@@ -1,20 +1,31 @@
 //import liraries
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
 
 // create a component
 class Contacts extends Component {
+    
+    onContactSelected(contact) {
+        console.log(contact);
+    }
     renderContact({ item }) {
-        return <Text>{item.name}</Text>;
+        return (
+            <View style={styles.contact}>
+                <TouchableOpacity onPress={this.onContactSelected.bind(this, item)} >
+                    <Text style={styles.name} >{item.name}</Text>
+                    <Text style={styles.phone}>{item.phone}</Text>
+                </TouchableOpacity>
+            </View>
+        );
     }
     render() {
         return (
             <View style={styles.container}>
                 <FlatList 
                     data={this.props.contacts}
-                    renderItem={this.renderContact}
-                    keyExtractor={item => item.id }
+                    renderItem={this.renderContact.bind(this)}
+                    keyExtractor={item => item.id}
                 />
             </View>
         );
@@ -26,6 +37,20 @@ const styles = StyleSheet.create({
     container: {
         flex: 1
     },
+    contact: {
+        flex: 1,
+        padding: 20,
+        borderColor: '#ddd',
+        borderBottomWidth: 1,
+        backgroundColor: '#fff'
+    },
+    name: {
+        fontSize: 16
+    },
+    phone: {
+        fontSize: 14,
+        color: '#ddd'
+    }
 });
 
 const mapStateToProps = state => {
